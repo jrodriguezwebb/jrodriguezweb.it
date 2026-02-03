eb8d98c7f0d7d147bede07bd3532d9a3fa05f359
jarodriguezp <jesus.jarp@gmail.com>
2026-02-03 23:00:36 +0100

    fix: solving issues with build

--- cv-app/src/app/[lang]/layout.tsx
+++ cv-app/src/app/[lang]/layout.tsx
@@ -1,8 +1,13 @@
 import "../globals.scss";
 import { metadata as meta } from "../layout";
 import { Metadata } from "next";
+import { i18n } from "../i18n-config";
 
 export const metadata: Metadata = meta;
+
+export async function generateStaticParams() {
+  return i18n.locales.map((lang) => ({ lang }));
+}
 
 export const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return <>{children}</>;

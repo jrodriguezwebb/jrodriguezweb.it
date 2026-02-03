f9c9e10044c40e22ff5eff36df771ebb6c42164b
jarodriguezp <jesus.jarp@gmail.com>
2026-02-03 22:20:03 +0100

    fix: adding favicon

--- cv-app/src/app/layout.tsx
+++ cv-app/src/app/layout.tsx
@@ -10,6 +10,9 @@ export const metadata: Metadata = {
   title: "jrodriguezweb.it",
   description:
     "I am Jesús Alfredo Rodríguez Pérez, a seasoned Full Stack Developer with over 13 years in web technology. My journey fuses frontend and backend development. Equipped with skills and innovation, I navigate diverse technologies like TypeScript, JavaScript, PHP, Angular, React, Ionic, NodeJS, and more. Crafting captivating user experiences focused on UI design, robust APIs, databases like MySQL, and Agile methodologies. As a Full Stack Developer, I unite both worlds, creating fluid connections and delivering end-to-end solutions. Proficient in English and Spanish, I effectively engage stakeholders and teams. My goal is to craft impactful web solutions with innovation and user-centricity.",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export const RootLayout = ({
